9310490296ffceb602e71d356d658d8735288419
Jashon Osala <jashonosala@gmail.com>
2022-11-19 03:17:56 +0300

    Format index.js

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-let run = false
+let run = false;
 let timerMillis = 0;
-let startBtn = document.getElementById("start_button");
-let stopBtn = document.getElementById("stop_button");
-let resetBtn = document.getElementById("reset_button");
+const startBtn = document.getElementById('start_button');
+const stopBtn = document.getElementById('stop_button');
+const resetBtn = document.getElementById('reset_button');
 
-startBtn.addEventListener("click", () => {
+startBtn.addEventListener('click', () => {
     run = true;
     runTimer();
 });
 
-stopBtn.addEventListener("click", () => {
+stopBtn.addEventListener('click', () => {
     run = false;
 });
 
-resetBtn.addEventListener("click", () => {
+resetBtn.addEventListener('click', () => {
     run = false;
     timerMillis = 0;
-    document.getElementById("milli_seconds").innerHTML = "000"
-})
+    document.getElementById('milli_seconds').innerHTML = '000';
+});
 
-function runTimer() {
+window.runTimer = function () {
     if (run) {
         timerMillis++;
         if (timerMillis >= 1000) {
-            timerMillis = 0
+            timerMillis = 0;
         }
 
         let timer = timerMillis < 10 ? `00${timerMillis}` : timerMillis < 100 ?
             `0${timerMillis}` : `${timerMillis}`;
 
-        document.getElementById("milli_seconds").innerHTML = timer;
-        setTimeout(runTimer, 10)
+        document.getElementById('milli_seconds').innerHTML = timer;
+        setTimeout(runTimer, 10);
     }
 }
