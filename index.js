2d070ef120938d969609e020e6dc2ce0172d1e28
Jashon Osala <jashonosala@gmail.com>
2022-11-29 10:22:20 +0300

    Js stopwatch update

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 let run = false;
 let timerMillis = 0;
 let timerSecs = 0;
+let [timerMins, timerHrs] = [0, 0];
 const startBtn = document.getElementById('start_button');
 const stopBtn = document.getElementById('stop_button');
 const resetBtn = document.getElementById('reset_button');
+const spinnerObj =  document.getElementById('spinner');
 
 const runTimer = () => {
     if (run) {
@@ -11,11 +13,14 @@ const runTimer = () => {
       const min = 60;
       const ten = 10;
       const one = 1;
+      const day = 24;
       const oneHundred = 100;
       let timer;
       let secs;
+      let mins;
+      let hrs;
 
-      timerMillis+=10;
+      timerMillis+=6;
       if (timerMillis >= maxTime) {
         timerMillis = 0;
         timerSecs++;
@@ -23,6 +28,16 @@ const runTimer = () => {
 
       if (timerSecs >= min) {
         timerSecs = 0;
+        timerMins++;
+      }
+
+      if (timerMins >= min){
+        timerMins = 0;
+        timerHrs++;
+      }
+
+      if (timerHrs >= day){
+        timerHrs = 0;
       }
 
       if (timerMillis < ten) {
@@ -40,26 +55,50 @@ const runTimer = () => {
         secs = `${timerSecs}`;
       }
 
+      if (timerMins < ten) {
+        mins = `0${timerMins}`;
+      }
+      else {
+        mins = `${timerMins}`;
+      }
+
+      if (timerHrs < ten) {
+        hrs = `0${timerHrs}`;
+      }
+      else {
+        hrs = `${timerHrs}`;
+      }
+
       document.getElementById('milli_seconds').innerHTML = timer;
       document.getElementById('seconds').innerHTML = secs;
+      document.getElementById('minutes').innerHTML = mins;
+      document.getElementById('hours').innerHTML = hrs;
 
       setTimeout(runTimer, one);
     }
   };
 
 startBtn.addEventListener('click', () => {
+  spinnerObj.className = 'xspinner';
+  spinnerObj.style.animationPlayState = 'running';
   run = true;
   runTimer();
 });
 
 stopBtn.addEventListener('click', () => {
+  spinnerObj.style.animationPlayState = 'paused';
   run = false;
 });
 
 resetBtn.addEventListener('click', () => {
   run = false;
   timerMillis = 0;
-  timerMillis = 0;
+  timerSecs = 0;
+  timerHrs = 0;
+  timerMins = 0;
   document.getElementById('milli_seconds').innerHTML = '000';
   document.getElementById('seconds').innerHTML = '00';
+  document.getElementById('minutes').innerHTML = '00';
+  document.getElementById('hours').innerHTML = '00';
+  spinnerObj.className = 'reset-spinner';
 });
